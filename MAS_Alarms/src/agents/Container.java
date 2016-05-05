5c8fc245cf121236bffb7050aa657652e8ce607c
Santiago Carmona Rincon <sacarmonar@gmail.com>
2016-05-05 09:41:23 -0500

    CREACION DE LA PLATAFORMA PRINCIPAL Y LOS AGENTES
    * Se crea la plataforma principal que es Container.java
    * Se crea la estructura básica de los 3 agentes
    * Ya son mostrados el la UI de JADE

---
+++ MAS_Alarms/src/agents/Container.java
@@ -0,0 +1,79 @@
+package agents;
+
+import jade.core.Profile;
+import jade.core.ProfileImpl;
+import jade.core.Runtime;
+import jade.wrapper.AgentController;
+import jade.wrapper.ContainerController;
+import jade.wrapper.ControllerException;
+
+public class Container {
+
+    final public int pi = 3;
+    String men;
+
+    public static void main(String[] args) {
+        new Container().crearContenedor();
+    }
+
+    public void crearContenedor() {
+        Runtime rt = Runtime.instance();
+
+        //Crear plataforma principal
+        Profile p = new ProfileImpl();
+        p.setParameter(Profile.MAIN_HOST, "localhost");
+        p.setParameter(Profile.MAIN_PORT, "1099");
+        p.setParameter(Profile.GUI, "true");
+        p.setParameter(Profile.PLATFORM_ID, "Plataforma");
+        p.setParameter(Profile.LOCAL_HOST, "localhost");
+        p.setParameter(Profile.AGENTS, "rma:jade.tools.rma.rma");
+        p.setParameter(Profile.CONTAINER_NAME, "ContenedorPrincipal");
+        ContainerController c = rt.createMainContainer(p);
+
+        //crear contenedor
+        Profile p2 = new ProfileImpl();
+        p.setParameter(Profile.MAIN_HOST, "localhost");
+        p.setParameter(Profile.MAIN_PORT, "1099");
+        p.setParameter(Profile.LOCAL_HOST, "localhost");
+        ContainerController contenedor = rt.createAgentContainer(p2);
+
+        try {
+            //Crear el agente
+            System.out.println(c.getContainerName().toString());
+            AgentController stud = c.createNewAgent("Estudiante", "agents.Student", null);
+            AgentController teac = c.createNewAgent("Profesor", "agents.Teacher", null);
+            AgentController team = c.createNewAgent("Equipo", "agents.Team", null);
+
+            team.start();
+            teac.start();
+            team.start();
+            
+            AgentController pi = c.getAgent("Equipo");
+
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            pi.putO2AObject(this, false);
+            System.out.println();
+
+
+
+            // try {
+            // ContainerController c = new ContainerController(cp, impl,
+            // platformName)
+            // .getPlatformController().kill();
+        } catch (ControllerException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    public void mostrarMensaje(String mensaje) {
+        System.out.println(mensaje);
+        men = mensaje;
+        System.out.println();
+    }
+}
---
+++ MAS_Alarms/src/agents/Student.java
@@ -0,0 +1,47 @@
+package agents;
+
+
+import jade.core.Agent;
+import jade.core.behaviours.CyclicBehaviour;
+
+public class Student extends Agent {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	Container contenedor;
+	int k;
+
+	protected void setup() {
+
+		// Printout a welcome message
+		System.out.println("Hola Mundo.. el agente: " + getAID().getName()
+				+ " esta listo.");
+
+		// Enable O2A communication
+		setEnabledO2ACommunication(true, 0);
+
+		// Add the behaviour serving notifications from the external system
+		this.addBehaviour(new CyclicBehaviour(this) {
+		
+			public void action() {
+				System.out.println("ENTRO AL COMPORTAMIENTO");
+				Container info = (Container) myAgent.getO2AObject();
+				if (info != null) {
+					contenedor = info;
+					contenedor.mostrarMensaje("Entrego mensaje al contenedor");
+					System.out.println();
+				} else {
+					block();
+				}
+			}
+		});
+
+	}
+
+	public void controla(Container cont) {
+		System.out.println("prueba");
+	}
+
+}
