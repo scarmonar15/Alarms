f3535b6eb2945c4d4520aaa9d2a0a6cbfff584d8
Santiago Carmona Rincon <sacarmonar@gmail.com>
2016-05-12 09:46:17 -0500

    Enviando el GET devuelve el JSON bien, falta parsearlo

--- MAS_Alarms/src/agents/Container.java
+++ MAS_Alarms/src/agents/Container.java
@@ -1,22 +1,53 @@
 package agents;
 
+import alarmsOntology.AlarmsOntology;
+import jade.content.lang.Codec;
+import jade.content.lang.sl.SLCodec;
+import jade.content.onto.Ontology;
+import jade.core.AID;
 import jade.core.Profile;
 import jade.core.ProfileImpl;
 import jade.core.Runtime;
+import jade.lang.acl.ACLMessage;
 import jade.wrapper.AgentController;
 import jade.wrapper.ContainerController;
 import jade.wrapper.ControllerException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Container {
+    static Teacher zapata;
+    static ContainerController c;
 
-    final public int pi = 3;
-    String men;
-
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         new Container().crearContenedor();
+        System.out.println("1. Denunciar a un estudiante \n2. Calificar una entrega");
+        while (true) {
+            BufferedReader buff = new BufferedReader(new InputStreamReader(System.in));
+            String respuesta = buff.readLine();
+            if (respuesta.equals("1")) {
+                new Container().denunciarEstudiante();
+                break;
+            } else if (respuesta.equals("2")) {
+                System.out.println("Has elegido calificar una entrega");
+                break;
+            } else {
+                System.out.println("El número seleccionado no se encuentra dentro de las opciones");
+            }
+        }
     }
-
-    public void crearContenedor() {
+    public void denunciarEstudiante(){
+        System.out.println("Has elegido Denunciar a un estudiante");
+        try{
+            AgentController prof = c.createNewAgent("Profesor2", "agents.Teacher", null);
+            prof.start();
+        } catch (ControllerException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+    public void crearContenedor() throws IOException {
         Runtime rt = Runtime.instance();
 
         //Crear plataforma principal
@@ -28,27 +59,17 @@ public class Container {
         p.setParameter(Profile.LOCAL_HOST, "localhost");
         p.setParameter(Profile.AGENTS, "rma:jade.tools.rma.rma");
         p.setParameter(Profile.CONTAINER_NAME, "ContenedorPrincipal");
-        ContainerController c = rt.createMainContainer(p);
-
-        //crear contenedor
-        Profile p2 = new ProfileImpl();
-        p.setParameter(Profile.MAIN_HOST, "localhost");
-        p.setParameter(Profile.MAIN_PORT, "1099");
-        p.setParameter(Profile.LOCAL_HOST, "localhost");
-        ContainerController contenedor = rt.createAgentContainer(p2);
+        c = rt.createMainContainer(p);
 
         try {
             //Crear el agente
-            System.out.println(c.getContainerName().toString());
             AgentController stud = c.createNewAgent("Estudiante", "agents.Student", null);
-            AgentController teac = c.createNewAgent("Profesor", "agents.Teacher", null);
+            //AgentController teac = c.createNewAgent("Profesor", "agents.Teacher", null);
             AgentController team = c.createNewAgent("Equipo", "agents.Team", null);
-
+            
             stud.start();
             team.start();
-            teac.start();
-            
-            AgentController pi = c.getAgent("Equipo");
+            //teac.start();
 
             try {
                 Thread.sleep(3000);
@@ -56,9 +77,6 @@ public class Container {
                 // TODO Auto-generated catch block
                 e.printStackTrace();
             }
-            pi.putO2AObject(this, false);
-            System.out.println();
-
 
 
             // try {
@@ -69,11 +87,6 @@ public class Container {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
-    }
-
-    public void mostrarMensaje(String mensaje) {
-        System.out.println(mensaje);
-        men = mensaje;
-        System.out.println();
+        
     }
 }
--- MAS_Alarms/src/agents/Teacher.java
+++ MAS_Alarms/src/agents/Teacher.java
@@ -19,6 +19,8 @@ import jade.content.lang.sl.*;
 import jade.content.onto.*;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
  
 public class Teacher extends Agent {
  
@@ -55,9 +57,7 @@ public class Teacher extends Agent {
                         System.out.println("Denuncia recibida");
                         System.out.println("Se ha hecho una denuncia de un estudiante"
                                 + " con cédula: " + e.getCedula() + " llamado " + e.getNombre() + " " + e.getApellido() + " con correo"
-                                + " " + e.getCorreo());
-
-                    }
+                                + " " + e.getCorreo());                    }
                     else{
                         // Recibido un INFORM con contenido incorrecto
                         ACLMessage reply = msg.createReply();
@@ -94,6 +94,7 @@ public class Teacher extends Agent {
   } //Fin de la clase WaitPingAndReplyBehaviour
     class ObtenerAlDenunciado extends SimpleBehaviour{
         private boolean finished = false;
+        private final String USER_AGENT = "Mozilla/5.0";
         public ObtenerAlDenunciado(Agent a) {
             super(a);
         }
@@ -111,6 +112,30 @@ public class Teacher extends Agent {
                 System.out.print("Ingrese cédula del estudiante que quiere denunciar: ");
                 BufferedReader buff = new BufferedReader(new InputStreamReader(System.in));
                 String respuesta = buff.readLine();
+                
+                String url = "http://alarms-api.herokuapp.com/students/" + respuesta.toString() + ".json";
+		URL obj = new URL(url);
+                HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+		// optional default is GET
+		con.setRequestMethod("GET");
+		//add request header
+		con.setRequestProperty("User-Agent", USER_AGENT);
+		int responseCode = con.getResponseCode();
+                System.out.println("\nSending 'GET' request to URL : " + url);
+                System.out.println("Response Code : " + responseCode);
+                
+                BufferedReader in = new BufferedReader(
+                        new InputStreamReader(con.getInputStream()));
+                String inputLine;
+                StringBuffer response = new StringBuffer();
+
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                in.close();
+
+                //print result
+                System.out.println(response);
                 
                 ObtenerEstudianteDenunciado oed = new ObtenerEstudianteDenunciado();
                 oed.setId_estudiante(Integer.parseInt(respuesta));
@@ -130,11 +155,13 @@ public class Teacher extends Agent {
     protected void setup() {
         getContentManager().registerLanguage(codec);
         getContentManager().registerOntology(ontologia);
-        WaitPingAndReplyBehaviour PingBehaviour;
+        
         ObtenerAlDenunciado SenderBehaviour;
         SenderBehaviour = new ObtenerAlDenunciado(this);
-        PingBehaviour = new  WaitPingAndReplyBehaviour(this);
         addBehaviour(SenderBehaviour);
+        
+        WaitPingAndReplyBehaviour PingBehaviour;
+        PingBehaviour = new  WaitPingAndReplyBehaviour(this);
         addBehaviour(PingBehaviour);
     }
 }
